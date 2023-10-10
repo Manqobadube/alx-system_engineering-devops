f7847d4ee4631ee40e1f133e5b2dc223e7845833
manqobadube <manqoba.dube7@gmail.com>
2023-10-10 11:43:34 -0700

    fixing more files

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
 """
-    This module contains a function that queries the
-    Reddit API and returns a list of the hottest post for
-    a given subreddit.
+Recursive function that queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit,
+the function should return None.
 """
+
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after=""):
     """
-        returns a list of the hottest posts of a subreddit
-        or None
+    Queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit.
+
+    - If not a valid subreddit, return None.
     """
-    u_agent = {'User-Agent': '/u/Suspicious-Jelly920'}
-    url = 'https://api.reddit.com/r/{}/hot?after={}'.format(subreddit, after)
-    res = requests.get(url,  headers=u_agent)
-    h_list = []
-    if res.status_code == 200:
-        hottest = res.json()["data"]["children"]
-        after = res.json()["data"]["after"]
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            hot_list.append(title)
+        after = req.json().get("data").get("after")
 
         if after is None:
-            h_list = get_children(hottest, len(hottest))
-            return h_list
-        h_list.append(recurse(subreddit, h_list, after=after))
-        h_list = get_children(hottest, len(hottest))
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
     else:
         return None
-    return h_list
-
-
-def get_children(h_list, count, new_hlist=[]):
-    """
-        gets the children from the data
-    """
-    if count == 0:
-        return new_hlist
-    new_hlist.append(h_list[count - 1]["data"]["title"])
-    return (get_children(h_list, count - 1, new_hlist))
